4efdcbd988c9870636dd826ae04687063ac8d50f
Neemias Amaro <74926184+neemiasamaro@users.noreply.github.com>
2022-05-20 18:26:45 -0300

    20/05

---
+++ Controllers/EstilosController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace Projeto_Tatuou.Controllers
+{
+
+    public class EstilosController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Estilos
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.Estilos.ToList());
+        }
+
+        [AllowAnonymous]
+        public ActionResult Estilos()
+        {
+            return View(db.Estilos.ToList());
+        }
+        // GET: Estilos/Details/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estilos estilos = db.Estilos.Find(id);
+            if (estilos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estilos);
+        }
+
+        // GET: Estilos/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estilos/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Descricao,Ativos")] Estilos estilos, HttpPostedFileBase arquivo)
+        {
+            string valor = "";
+            if (ModelState.IsValid)
+            {
+                db.Estilos.Add(estilos);
+                db.SaveChanges();
+                if (arquivo != null)
+                {
+                    string nomearq = estilos.Id + estilos.Nome + ".png";
+                    valor = Funcoes.UploadEstilos(arquivo, nomearq);
+                    if (valor == "sucesso")
+                    {
+                        estilos.Foto = nomearq;
+                        db.Entry(estilos).State = EntityState.Modified;
+                        db.SaveChanges();
+                        TempData["MSG"] = "success|Usuário cadastrado com sucesso";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        System.IO.File.Copy(Request.PhysicalApplicationPath + "images\\user.png", Request.PhysicalApplicationPath + "images\\estilos\\" + estilos.Id + estilos.Nome + ".png");
+                        estilos.Foto = estilos.Id + estilos.Nome + ".png";
+                        db.Entry(estilos).State = EntityState.Modified;
+                        db.SaveChanges();
+                        TempData["MSG"] = "warning|Problema no Upload da foto: " + valor;
+                        return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    System.IO.File.Copy(Request.PhysicalApplicationPath + "images\\user.png", Request.PhysicalApplicationPath + "images\\estilos\\" + estilos.Id + estilos.Nome + ".png");
+                    estilos.Foto = estilos.Id + estilos.Nome + ".png";
+                    db.Entry(estilos).State = EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["MSG"] = "success|Usuário cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        // GET: Estilos/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estilos estilos = db.Estilos.Find(id);
+            if (estilos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estilos);
+        }
+
+        // POST: Estilos/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome, Descricao,Ativos")] Estilos estilos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estilos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estilos);
+        }
+
+        // GET: Estilos/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estilos estilos = db.Estilos.Find(id);
+            if (estilos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estilos);
+        }
+
+        // POST: Estilos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estilos estilos = db.Estilos.Find(id);
+            db.Estilos.Remove(estilos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,44 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.Models.Contexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            SetSqlGenerator("MySql.Data.MySqlClient", new
+            MySql.Data.EntityFramework.MySqlMigrationSqlGenerator());
+        }
+
+        protected override void Seed(WebApplication1.Models.Contexto context)
+        {
+            context.Usuario.AddOrUpdate(
+              p => p.Email,
+              new Models.Usuario { Id = 1, Nome = "Administrador", Email = "tatuouadmin@gmail.com", Senha = "V8SLtjr9Lz6lYIiywHSnaYNNwbbZ3l9lKJ8sPAomC8YyUeiO+0G4SWvLWhjHw9Z0cNwkeNT/wRuSmRs0ke874A==", Status = true, PerfilId = 1 },
+              new Models.Usuario { Id = 2, Nome = "Neemias", Email = "nemisilva10@gmail.com", Senha = "V8SLtjr9Lz6lYIiywHSnaYNNwbbZ3l9lKJ8sPAomC8YyUeiO+0G4SWvLWhjHw9Z0cNwkeNT/wRuSmRs0ke874A==", Status = true, PerfilId = 2 });
+            context.Perfil.AddOrUpdate(
+                p => p.Descricao,
+                new Models.Perfil { Id = 1, Descricao = "Admin" },
+                new Models.Perfil { Id = 2, Descricao = "Comum" });
+            context.Estilos.AddOrUpdate(
+                p => p.Nome,
+                new Models.Estilos { Id = 1, Nome = "Tribal", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 2, Nome = "Tribal2", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 3, Nome = "Tribal3", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 4, Nome = "Tribal4", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 5, Nome = "Tribal5", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 6, Nome = "Tribal6", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 7, Nome = "Tribal7", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 8, Nome = "Tribal8", Descricao = "Teste", Ativos = true },
+                new Models.Estilos { Id = 9, Nome = "Tribal9", Descricao = "Teste", Ativos = true });
+            context.Estudio.AddOrUpdate(
+                p => p.nomeEstudio,
+                new Models.Estudio { Id = 1, nomeEstudio = "Teste", Cep = "12518250", Cidade = "Guaratinguetá", Logradouro = "Avenida Paulo Geraldo Pinto", Bairro = "Jardim Esperança", Cnpj = "11111111111111", Disponivel = true, Numero = "165", UsuarioId = 2 });
+        }
+    }
+}
