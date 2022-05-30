197589c7580df69121478c8774ffbbf941842382
Neemias <nemisilva10@gmail.com>
2022-05-30 14:51:02 -0300

    Ajustes Views

---
+++ Projeto-Tatuou/Scripts/confirmador.js
@@ -0,0 +1,6 @@
+﻿function confirma(id) {
+    let resposta = confirm("Tem certeza que deseja excluir este item?")
+    if (resposta == true) {
+        window.location.href = "Estilos/Delete/" + id;
+    }
+}
